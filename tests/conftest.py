6155a9cdaed5510deceea3e62b9ad2e4e2f7e6a8
Jakub3628800 <JakubyKriz@gmail.com>
2021-05-24 00:20:05 +0200

    set up short url endpoints

---
+++ tests/conftest.py
@@ -0,0 +1,8 @@
+import pytest
+from starlette.testclient import TestClient
+from shortener.factory import create_app
+
+@pytest.fixture
+def test_client():
+    app = create_app()
+    return TestClient(app)
---
+++ tests/integration/test_basic.py
@@ -0,0 +1,11 @@
+
+def test_ping(test_client):
+    response = test_client.get("/ping")
+    assert response.status_code == 200
+    assert response.json() == {"ping": "pong"}
+
+
+def test_status(test_client):
+    response = test_client.get("/status")
+    assert response.status_code == 200
+    assert response.json() == {'db_up': 'false'}
