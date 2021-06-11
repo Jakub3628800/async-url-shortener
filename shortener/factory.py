bf1aa4131ef8dc1b84b9e3f33f16f9209bb5b1e2
Jakub3628800 <JakubyKriz@gmail.com>
2021-06-11 23:01:56 +0200

    make db actions work

--- shortener/factory.py
+++ shortener/factory.py
@@ -1,10 +1,13 @@
-from starlette.applications import Starlette
 from starlette.routing import Mount, Route
-
+from shortener.actions import UrlNotFoundException
 from shortener.database import set_up_postgres_connection, tear_down_postgres_connection
 from shortener.views.basic import ping, status
 from shortener.views.urls import routes as url_routes
 from shortener.views.redirect import redirect_url
+
+from starlette.applications import Starlette
+from starlette.responses import JSONResponse
+from starlette.exceptions import HTTPException
 
 routes = [
     Route("/ping", ping),
@@ -14,8 +17,23 @@ routes = [
 ]
 
 
+async def server_error(request, exc):
+    return JSONResponse({"Internal server error": "ok"}, status_code=500)
+
+
+async def not_found(request, exc):
+    return JSONResponse({"Resource not found": "ok"}, status_code=404)
+
+
+exception_handlers = {HTTPException: server_error, UrlNotFoundException: not_found}
+
+
 def create_app():
     """Create application object."""
     return Starlette(
-        debug=True, routes=routes, on_startup=[set_up_postgres_connection], on_shutdown=[tear_down_postgres_connection]
+        debug=True,
+        routes=routes,
+        on_startup=[set_up_postgres_connection],
+        on_shutdown=[tear_down_postgres_connection],
+        exception_handlers=exception_handlers,
     )
