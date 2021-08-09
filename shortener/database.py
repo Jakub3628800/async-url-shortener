067d8cd240db99c138a7e6a4eb616dc2677241ea
Jakub3628800 <JakubyKriz@gmail.com>
2021-08-09 23:33:49 +0200

    add environment to github actions

--- shortener/database.py
+++ shortener/database.py
@@ -2,16 +2,21 @@ from databases import Database
 from pydantic import SecretStr
 from typing import Optional
 import asyncpg
+import os
 import asyncio
 from contextlib import asynccontextmanager
 
-def load_database_url() -> SecretStr:
-    db_host = "localhost:5432"
-    db_name = "urldatabase"
-    db_user = "localuser"
-    db_password = SecretStr("password123")
 
-    db_url = f"postgresql://{db_user}:{db_password.get_secret_value()}@{db_host}/{db_name}"
+def load_database_url() -> SecretStr:
+    db_port = os.getenv("DB_PORT", 5432)
+    db_host = os.getenv("DB_HOST", "localhost")
+
+    db_hostname = f"{db_host}:{db_port}"
+    db_name = os.getenv("DB_NAME","postgres")
+    db_user = os.getenv("DB_USER","localuser")
+    db_password = SecretStr(os.getenv("DB_PASS","password123"))
+
+    db_url = f"postgresql://{db_user}:{db_password.get_secret_value()}@{db_hostname}/{db_name}"
     return SecretStr(db_url)
 
 
