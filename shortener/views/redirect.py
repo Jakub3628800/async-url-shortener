51f1ae9984c5f7ec7367d3dc92dae5e18d8d6f89
Jakub3628800 <JakubyKriz@gmail.com>
2021-08-10 19:50:06 +0200

    add openapi docs to endpoints

--- shortener/views/redirect.py
+++ shortener/views/redirect.py
@@ -3,7 +3,18 @@ from shortener.actions import get_url_target
 
 
 async def redirect_url(request):
-    """Status request to check service has a connection to the database."""
+    """
+    summary: Redirect request to a target url.
+    parameters:
+        - name: short_url
+          in: path
+          required: true
+          schema:
+            type : string
+    responses:
+      200:
+        description: Redirected to target url.
+    """
     short_url = request.get("path_params", {}).get("short_url")
 
     target_url = await get_url_target(short_url)
