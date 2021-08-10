b04602153c20bb695e6e95cab828a777e94d3bfe
Jakub3628800 <JakubyKriz@gmail.com>
2021-08-10 13:52:53 +0200

    add more integration tests

--- run_app.py
+++ run_app.py
@@ -1,8 +1,6 @@
 import uvicorn
-import asyncio
-from shortener.factory import init_app
+from shortener.factory import app
+
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    app = loop.run_until_complete(init_app())
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000, loop="uvloop")
