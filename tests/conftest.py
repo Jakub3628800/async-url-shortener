1b53146bb8dba190ab55f2fec54cf8133e0a9a2e
Jakub3628800 <JakubyKriz@gmail.com>
2021-08-10 08:14:37 +0200

    fix database connections in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+import os
 from starlette.testclient import TestClient
 from shortener.factory import init_app
 from shortener.database import create_db_pool
@@ -12,20 +13,33 @@ def test_client():
     app = loop.run_until_complete(init_app())
     return TestClient(app=app)
 
-@pytest.fixture()
+@pytest.fixture(scope="session")
 def psycopg2_cursor():
     import psycopg2
 
+    db_port = os.getenv("DB_PORT", 5432)
+    db_host = os.getenv("DB_HOST", "localhost")
+
+    db_name = os.getenv("DB_NAME","postgres")
+    db_user = os.getenv("DB_USER","localuser")
+    db_password = os.getenv("DB_PASS","password123")
     connection = psycopg2.connect(
-        dbname="urldatabase",
-        user="localuser",
-        password="password123",
-        host="localhost",
-        port=5432
+        dbname=db_name,
+        user=db_user,
+        password=db_password,
+        host=db_host,
+        port=db_port
     )
 
 
     cur = connection.cursor()
     cur.execute('SELECT 1')
+
+    migration_files = ["create_short_url_table.sql"]
+    for filename in migration_files:
+        with open(f"database_migrations/{filename}") as sql_file:
+            migration = "".join(sql_file.readlines())
+            cur.execute(query=migration)
+            connection.commit()
     yield cur
     connection.close()
