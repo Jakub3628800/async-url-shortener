dee3109eda54291366e8e7ebc5528e7ef5d66db1
Jakub3628800 <JakubyKriz@gmail.com>
2021-08-10 08:21:34 +0200

    run pre-commit

--- shortener/database.py
+++ shortener/database.py
@@ -1,10 +1,5 @@
-from databases import Database
 from pydantic import SecretStr
-from typing import Optional
-import asyncpg
 import os
-import asyncio
-from contextlib import asynccontextmanager
 
 
 def load_database_url() -> SecretStr:
@@ -12,33 +7,9 @@ def load_database_url() -> SecretStr:
     db_host = os.getenv("DB_HOST", "localhost")
 
     db_hostname = f"{db_host}:{db_port}"
-    db_name = os.getenv("DB_NAME","postgres")
-    db_user = os.getenv("DB_USER","localuser")
-    db_password = SecretStr(os.getenv("DB_PASS","password123"))
+    db_name = os.getenv("DB_NAME", "postgres")
+    db_user = os.getenv("DB_USER", "localuser")
+    db_password = SecretStr(os.getenv("DB_PASS", "password123"))
 
     db_url = f"postgresql://{db_user}:{db_password.get_secret_value()}@{db_hostname}/{db_name}"
     return SecretStr(db_url)
-
-
-class DatabaseConnector:
-
-    pool: Optional[asyncpg.Pool] = None
-
-    async def init_pool(self):
-       self.pool = await asyncpg.create_pool(dsn=load_database_url().get_secret_value(), min_size=5, max_size=25, loop=asyncio.get_running_loop())
-
-    @asynccontextmanager
-    async def connection(self):
-        if self.pool is None:
-            await self.init_pool()
-        async with self.pool.acquire() as conn:
-            yield conn
-
-import asyncpg
-
-async def create_db_pool():
-    pool = asyncpg.create_pool(dsn=load_database_url().get_secret_value(), min_size=5, max_size=25)
-    return pool
-
-
-
--- tests/conftest.py
+++ tests/conftest.py
@@ -2,9 +2,7 @@ import pytest
 import os
 from starlette.testclient import TestClient
 from shortener.factory import init_app
-from shortener.database import create_db_pool
 import asyncio
-import httpx
 
 
 @pytest.fixture(scope="function")
@@ -13,6 +11,7 @@ def test_client():
     app = loop.run_until_complete(init_app())
     return TestClient(app=app)
 
+
 @pytest.fixture(scope="session")
 def psycopg2_cursor():
     import psycopg2
@@ -20,26 +19,21 @@ def psycopg2_cursor():
     db_port = os.getenv("DB_PORT", 5432)
     db_host = os.getenv("DB_HOST", "localhost")
 
-    db_name = os.getenv("DB_NAME","postgres")
-    db_user = os.getenv("DB_USER","localuser")
-    db_password = os.getenv("DB_PASS","password123")
-    connection = psycopg2.connect(
-        dbname=db_name,
-        user=db_user,
-        password=db_password,
-        host=db_host,
-        port=db_port
-    )
-
+    db_name = os.getenv("DB_NAME", "postgres")
+    db_user = os.getenv("DB_USER", "localuser")
+    db_password = os.getenv("DB_PASS", "password123")
+    connection = psycopg2.connect(dbname=db_name, user=db_user, password=db_password, host=db_host, port=db_port)
 
     cur = connection.cursor()
-    cur.execute('SELECT 1')
+    cur.execute("SELECT 1")
 
-    migration_files = ["create_short_url_table.sql"]
-    for filename in migration_files:
-        with open(f"database_migrations/{filename}") as sql_file:
+    try:
+        with open("database_migrations/create_short_url_table.sql") as sql_file:
             migration = "".join(sql_file.readlines())
             cur.execute(query=migration)
-            connection.commit()
+    except Exception:
+        pass
+
+    connection.commit()
     yield cur
     connection.close()
