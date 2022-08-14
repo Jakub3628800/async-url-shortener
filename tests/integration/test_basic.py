75d825993665f52b5e10adcfaf910b424badc9e7
jk <jakubykriz@gmail.com>
2022-08-14 22:58:06 +0200

    chore: update pytest-asyncio

--- tests/integration/test_basic.py
+++ tests/integration/test_basic.py
@@ -1,14 +1,9 @@
-import pytest
-
-
-@pytest.mark.asyncio
 def test_ping(test_client):
     response = test_client.get("/ping")
     assert response.status_code == 200
     assert response.json() == {"ping": "pong"}
 
 
-@pytest.mark.asyncio
 def test_status(test_client):
     response = test_client.get("/status")
     assert response.status_code == 200
