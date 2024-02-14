f99229a3fdf08e6ceb96081729fb3e4eea1df4e7
jk <jakubykriz@gmail.com>
2024-02-14 22:25:08 +0100

    add hello world fastapi app

--- run_app.py
+++ run_app.py
@@ -2,8 +2,8 @@ import os
 
 import uvicorn
 
-#from shortener.factory import app
-from shortener_fastapi.main import app
+from shortener.factory import app
+from shortener_fastapi.main import app as app_fastapi  # noqa: F401
 
 if __name__ == "__main__":
     port = os.getenv("APPLICATION_PORT", 8000)
