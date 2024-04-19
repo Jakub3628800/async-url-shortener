7a45a3aa5b728087ae0323932b8203f52e976c45
jk <jakubykriz@gmail.com>
2024-04-19 22:36:41 +0200

    ruff reformat

--- shortener/actions.py
+++ shortener/actions.py
@@ -20,7 +20,9 @@ async def check_db_up(connection: asyncpg.Connection) -> bool:
 
 async def get_url_target(short_url: str, connection: asyncpg.Connection) -> str:
     try:
-        return await connection.fetchval("SELECT target from short_urls where url_key=$1;", short_url)
+        return await connection.fetchval(
+            "SELECT target from short_urls where url_key=$1;", short_url
+        )
     except Exception:
         raise UrlNotFoundException()
 
@@ -29,17 +31,31 @@ async def get_all_short_urls(connection: asyncpg.Connection):
     await connection.fetchval(query="SELECT * from short_urls;")
 
 
-async def create_url_target(short_url: str, target_url: str, connection: asyncpg.Connection):
+async def create_url_target(
+    short_url: str, target_url: str, connection: asyncpg.Connection
+):
     try:
-        await connection.execute("INSERT INTO short_urls (url_key, target) VALUES ($1,$2);", short_url, target_url)
+        await connection.execute(
+            "INSERT INTO short_urls (url_key, target) VALUES ($1,$2);",
+            short_url,
+            target_url,
+        )
     except asyncpg.exceptions.UniqueViolationError:
         pass
     return
 
 
-async def update_url_target(short_url: str, new_target_url: str, connection: asyncpg.Connection):
-    return await connection.fetchval("UPDATE short_urls SET target = $1 WHERE url_key = $2;", new_target_url, short_url)
+async def update_url_target(
+    short_url: str, new_target_url: str, connection: asyncpg.Connection
+):
+    return await connection.fetchval(
+        "UPDATE short_urls SET target = $1 WHERE url_key = $2;",
+        new_target_url,
+        short_url,
+    )
 
 
 async def delete_url_target(short_url: str, connection: asyncpg.Connection):
-    return await connection.fetchval("delete from short_urls where url_key=$1;", short_url)
+    return await connection.fetchval(
+        "delete from short_urls where url_key=$1;", short_url
+    )
--- shortener/settings.py
+++ shortener/settings.py
@@ -4,7 +4,11 @@ from pydantic_settings import BaseSettings
 class PostgresSettings(BaseSettings):
     """Settings for postgres connection."""
 
-    model_config = {"env_prefix": "DB_", "env_file": ".env", "env_file_encoding": "utf-8"}
+    model_config = {
+        "env_prefix": "DB_",
+        "env_file": ".env",
+        "env_file_encoding": "utf-8",
+    }
 
     host: str = "localhost"
     port: int = 5432
