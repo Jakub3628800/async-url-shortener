21cc3417e1434baa3920f72ace8f599f2c7eca10
Jakub Kriz <jakubykriz@gmail.com>
2024-09-21 09:26:18 +0200

    fix: mypy in pre-commit

--- run_app.py
+++ run_app.py
@@ -6,4 +6,4 @@ from shortener.factory import app
 
 if __name__ == "__main__":
     port = os.getenv("APPLICATION_PORT", 8000)
-    uvicorn.run(app, host="127.0.0.0", port=port, loop="uvloop")
+    uvicorn.run(app, host="127.0.0.0", port=int(port), loop="uvloop")
