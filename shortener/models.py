72a531d20f59bf7bb942856482f9c83a5078eae0
Jakub Kriz <jakubykriz@gmail.com>
2025-08-10 19:20:26 +0200

    Simplify models structure by consolidating into single file

--- shortener/models.py
+++ shortener/models.py
@@ -1,7 +1,9 @@
 from sqlalchemy import Column, DateTime, Integer, String
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.sql import func
 
-from ..models import Base
+Base = declarative_base()
+
 
 class ShortUrl(Base):  # type: ignore
     __tablename__ = 'short_urls'
@@ -10,3 +12,6 @@ class ShortUrl(Base):  # type: ignore
     url_key = Column(String, unique=True, nullable=False)
     target = Column(String, nullable=False)
     created_at = Column(DateTime, server_default=func.now(), nullable=False)
+
+
+__all__ = ["Base", "ShortUrl"]
--- shortener/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy.orm import declarative_base
-
-Base = declarative_base()
-
-from .url import ShortUrl  # noqa: E402
-
-__all__ = ["Base", "ShortUrl"]
